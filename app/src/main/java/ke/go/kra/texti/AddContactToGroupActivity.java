c9d3b44054e8a1df120c5000fbe1c87c9abe3e2c
Daniel Chang'masa <danchangmasa@gmail.com>
2017-07-20 08:19:00 -0700

    Improvements to the app

--- app/src/main/java/ke/go/kra/texti/AddContactToGroupActivity.java
+++ app/src/main/java/ke/go/kra/texti/AddContactToGroupActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import java.util.ArrayList;
+
 
 public class AddContactToGroupActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact_to_group);
+
+        ArrayList<>
     }
 }
 
